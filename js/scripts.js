77d0889eb917fa9574a16a6bcd2bb75c0d59a799
olga-aspro <82937552+olga-aspro@users.noreply.github.com>
2021-06-16 12:50:47 +0200

    2.5: Code Quality & Cross-Browser Testing
    1. Adjust the color for the header menu
    2. Add link to Projects
    3. defined the language for the website

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log("Welcome to My Portfolio Site");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && $(window).width() < 640) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
