e3937100de211bc9117c9e91dd2d593ddd94d8ce
olga-aspro <82937552+olga-aspro@users.noreply.github.com>
2021-06-17 11:57:29 +0200

    Linters corrections
    1. Linters correction
    2. Fixed menu for work on mobile version to close by click on the link.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Welcome to My Portfolio Site");
+console.log('Welcome to My Portfolio Site')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle' && $(window).width() < 640) {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
